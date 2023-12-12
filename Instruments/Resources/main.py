d3a9ba2afff036bd2983d64d4e73a91d0ca7a74c
Danntk <cposhtaa@gmail.com>
2023-12-12 16:09:56 +0100

    + Shop localization

--- Instruments/Resources/main.py
+++ Instruments/Resources/main.py
@@ -110,5 +110,6 @@ create_registration_resources()
 create_prices_resource()
 create_settings_resources()
 create_currency_setter_resources()
+create_shop_resources()
 
 print(f"Resources are loaded")
