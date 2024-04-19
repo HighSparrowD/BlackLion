44e86bb75b8c43d4a02b345cb0def3cf21b8e094
negogv <cposhtaa@gmail.com>
2024-04-19 14:10:32 +0200

    + recent reports menu

--- FrontEnd/AdminModule.py
+++ FrontEnd/AdminModule.py
@@ -17,10 +17,13 @@ class AdminModule(Personality_Bot):
             self.destruct()
             return
 
-        self.recent_updates = self.api_service.get_recent_updates()
 
         self.current_callback_handler = self.bot.register_callback_query_handler(message, self.callback_handler,
                                                                                  user_id=self.current_user)
+        self.recent_updates = self.api_service.get_recent_updates()
+        self.recent_reports_list = None
+
+        self.reports_calldata: bool = False
 
         self.start_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Recent feedbacks', callback_data='1'),
                                                    InlineKeyboardButton(f'{self.recent_updates.recentFeedbackCount}', callback_data='1')],
@@ -32,13 +35,32 @@ class AdminModule(Personality_Bot):
                                                    InlineKeyboardButton(f'{self.recent_updates.pendingAdvertisementCount}', callback_data='4')],
                                                   [InlineKeyboardButton('Pending Adventures', callback_data='5'),
                                                    InlineKeyboardButton(f'{self.recent_updates.pendingAdventureCount}', callback_data='5')],
-                                                  [InlineKeyboardButton('Exit to main menu', callback_data='0')]])
+                                                  [InlineKeyboardButton('Exit to main menu', callback_data='a')]])
 
         self.start()
 
+    def recent_reports_markup(self):
+        markup = InlineKeyboardMarkup([[InlineKeyboardButton(f'Report #{button_data.id}', callback_data=f'{button_data.id}')]
+                                       for button_data in self.api_service.get_recent_reports()])
+        markup.add(InlineKeyboardButton('Go back', callback_data='a'))
+        return markup
+
     def start(self):
         self.send_active_message('Check the recent updates!', self.start_markup)
+        self.reports_calldata = False
+
+    def recent_reports(self):
+        self.prev_func = self.start
+        self.reports_calldata = True
+
+        self.send_active_message('Recent reports:', self.recent_reports_markup())
 
     def callback_handler(self, call: CallbackQuery):
-        if call.data == '0':
+        if call.data == 'a':
             self.prev_menu()
+        elif self.reports_calldata:
+            pass
+        elif call.data == '2':
+            self.recent_reports()
+        else:
+            self.send_error_message("This feature isn't ready")
--- FrontEnd/Models/Admin/Admin.py
+++ FrontEnd/Models/Admin/Admin.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class RecentUpdates:
     def __init__(self, data_dict):
         self.recentFeedbackCount: int = data_dict["recentFeedbackCount"]
@@ -5,3 +8,18 @@ class RecentUpdates:
         self.verificationRequestCount: int = data_dict["verificationRequestCount"]
         self.pendingAdvertisementCount: int = data_dict["pendingAdvertisementCount"]
         self.pendingAdventureCount: int = data_dict["pendingAdventureCount"]
+
+class RecentReports:
+    def __init__(self, data_dict):
+        self.id: int = data_dict['id']
+        self.senderId: int = data_dict['senderId']
+        self.userId: int = data_dict['userId']
+        self.adventureId: int = data_dict['adventureId']
+        self.adminId: int = data_dict['adminId']
+        self.text: str = data_dict['text']
+        self.reason: str = data_dict['reason']
+        self.insertedUtc: datetime.datetime = data_dict['insertedUtc']
+
+    @staticmethod
+    def unpack(datas) -> list[any] | None:
+        return [RecentReports(data_dict) for data_dict in datas]
